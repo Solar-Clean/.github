2228e755874d55d890457882794af0607ba7d8bd
ReykCS <reyk-carstens@web.de>
2023-07-12 10:24:52 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -41,35 +41,10 @@ else
   exit 1
 fi
 
-sudo add-apt-repository universe
-sudo add-apt-repository multiverse
-sudo add-apt-repository restricted
-sudo apt update
-
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-
-sudo apt-get -y install ros-noetic-desktop-full
-
-## Add ros to bashrc
-echo "source /opt/ros/noetic/setup.${CURSHELL}" >> ~/.${CURSHELL}rc
-source ~/.${CURSHELL}rc 
-
 sudo apt-get -y install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential python3-pip
 
 sudo rosdep init 
 rosdep update
-
-sudo apt-get update && sudo apt-get -y install \
-    libopencv-dev \
-    liblua5.2-dev \
-    screen \
-    python3-rospkg-modules \
-    ros-noetic-navigation \
-    ros-noetic-teb-local-planner \
-    ros-noetic-mpc-local-planner \
-    libarmadillo-dev \
-    ros-noetic-nlopt
 
 pip install poetry
 source ~/.${CURSHELL}rc
@@ -82,14 +57,17 @@ rosws update
 cp pyproject.toml ../../pyproject.toml
 
 cd ../..
+
 poetry install
 poetry update
 
 poetry run catkin_make -DCMAKE_CXX_STANDARD=14
 
-echo "alias make_solarswarm='catkin_make -C ${PWD} -DCMAKE_CXX_STANDARD=14'" >> ~/.${CURSHELL}_aliases
+
+echo "alias make_solarswarm='catkin_make -C ${PWD}'" >> ~/.${CURSHELL}_aliases
 echo "alias workon_solarswarm='cd ${PWD} && poetry shell'" >> ~/.${CURSHELL}_aliases
-echo "alias solarswarm='source ${PWD}/devel/setup.sh'" >> ~/.${CURSHELL}_aliases
+
+echo "source ${PWD}/devel/setup.sh" >> ~/.${CURSHELL}rc
 
 source ~/.${CURSHELL}rc
 
